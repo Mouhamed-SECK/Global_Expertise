033ac055f6c3a4a5da13be2afbf31e14faafe6b4
medsoseck <medsoseck@gmail.com>
2021-01-21 20:40:56 +0100

    feat: add migrations for new models

---
+++ GlobalExpertise/Migrations/202101211502222_custumerEmployeeOrder.cs
@@ -0,0 +1,61 @@
+﻿namespace GlobalExpertise.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class custumerEmployeeOrder : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customer",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        FirstName = c.String(nullable: false, maxLength: 150),
+                        Email = c.String(nullable: false, maxLength: 150),
+                        Address = c.String(nullable: false, maxLength: 50),
+                        IdCardNumber = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Order",
+                c => new
+                    {
+                        OrderId = c.Int(nullable: false, identity: true),
+                        OrderNumber = c.String(),
+                        Status = c.Int(nullable: false),
+                        orderedDate = c.DateTime(nullable: false),
+                        CustomerID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.OrderId)
+                .ForeignKey("dbo.Customer", t => t.CustomerID, cascadeDelete: true)
+                .Index(t => t.CustomerID);
+            
+            CreateTable(
+                "dbo.Employee",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        FirstName = c.String(nullable: false, maxLength: 150),
+                        Email = c.String(nullable: false, maxLength: 150),
+                        Password = c.String(nullable: false, maxLength: 150),
+                        Departement = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Order", "CustomerID", "dbo.Customer");
+            DropIndex("dbo.Order", new[] { "CustomerID" });
+            DropTable("dbo.Employee");
+            DropTable("dbo.Order");
+            DropTable("dbo.Customer");
+        }
+    }
+}
