17f461741ce62b35f14336066ebb3b173d091a3b
medsoseck <medsoseck@gmail.com>
2021-01-21 20:40:19 +0100

    feat : add data access layer with global contex and fixtures

---
+++ GlobalExpertise/DAL/EnterpriseContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace GlobalExpertise.DAL
+{
+    class EnterpriseContext : DbContext
+    {
+        public EnterpriseContext() : base("EnterpriseContext")
+        {
+        }
+        public DbSet<models.Employee> employees { get; set; }
+        public DbSet<models.Customer> customers { get; set; }
+        public DbSet<models.Order> orders { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+
+
+
+
+}
---
+++ GlobalExpertise/DAL/Initializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using   GlobalExpertise.models;
+
+namespace GlobalExpertise.DAL 
+{
+    class Initializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<EnterpriseContext>
+    {
+        protected override void Seed(EnterpriseContext context)
+        {
+            var customers = new List<Customer>
+            {
+            new Customer{Name="Carson",FirstName="Alexander",Email="customer@gmail.com",Address= "Km unite 5 villa 586", IdCardNumber="44852765668658"},
+            new Customer{Name="Carson",FirstName="Alexander",Email="customer@gmail.com",Address= "Km unite 5 villa 586", IdCardNumber="44852765668658"},
+            new Customer{Name="Carson",FirstName="Alexander",Email="customer@gmail.com",Address= "Km unite 5 villa 586", IdCardNumber="44852765668658"},
+            new Customer{Name="Carson",FirstName="Alexander",Email="customer@gmail.com",Address= "Km unite 5 villa 586", IdCardNumber="44852765668658"},
+            };
+            customers.ForEach(s => context.customers.Add(s));
+            context.SaveChanges();
+
+            var employees = new List<Employee>
+            {
+            new Employee{Name="Carson",FirstName="Alexander",Email="customer@gmail.com",Password= "passer", Departement= DEPARTEMENT.Commande},
+            new Employee{Name="Carson",FirstName="Alexander",Email="customer@gmail.com",Password= "passer", Departement= DEPARTEMENT.Admin},
+            new Employee{Name="Carson",FirstName="Alexander",Email="customer@gmail.com",Password= "passer", Departement= DEPARTEMENT.Livraison},
+            new Employee{Name="Carson",FirstName="Alexander",Email="customer@gmail.com",Password= "passer", Departement=DEPARTEMENT.Comptable},
+            };
+            employees.ForEach(s => context.employees.Add(s));
+            context.SaveChanges();
+
+
+        }
+
+    }
+}
